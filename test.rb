714836385d20649cc39504412e42f7a2a25d9947
MarcosCast <marcosmax.5@gmail.com>
2023-03-23 08:56:34 -0300

    Realinhando

--- test.rb
+++ test.rb
@@ -1,25 +1,6 @@
-require 'open-uri'
+puts "Hello World!"
 
-class ExampleClass < AnotherClass
-    attr_reader :foo, :bar
-    attr_writer :baz, :ban
-    attr_accessor :boom
+cidade = "Brasília"
 
-    def initialize
-        a_method(argi)
-    end
+puts cidade
 
-    def a_method(argi)
-        hello_world = 10
-        hello_world = += 1
-        SomeClass::AnotherClass.new(key: 10, another_key: "value")
-        return :some_symbol
-
-        ['ab', 'cd'].each do |item|
-            puts item
-        end
-    end
-end
-
-s = ExampleClass.new
-s.a_method(:foo)
