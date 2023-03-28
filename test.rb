e09de655ead77f5685f583d7cfd7a140bd5729dc
MarcosCast <marcosmax.5@gmail.com>
2023-03-28 16:22:15 -0300

    Criando arrays com dados

--- test.rb
+++ test.rb
@@ -1,5 +1,17 @@
+
+
+@agenda = [
+    {
+        nome: "Marcos", telefone: "123456789"
+    },
+    {
+        nome: "Samuel", telefone: "987654321"
+    }
+]
+
+
 loop do
-    puts "1. Contatos\n2. Adicionar Contato\n3. Ver Contato\n4. Editar Contato\n5. Remover Contato \n0, Sair"
+    puts "1. Contatos\n2. Adicionar Contato\n3. Ver Contato\n4. Editar Contato\n5. Remover Contato \n0. Sair"
          cod = gets.chomp.to_i
 
          case 
